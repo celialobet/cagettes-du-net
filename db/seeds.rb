125f0f6190cb7ff5965274c7b7b2e8e982414904
Bapt <baptiste.buch@gmail.com>
2021-06-15 17:23:12 +0200

    add seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+User.destroy_all
+Basket.destroy_all
+AdditionalProducts.destroy_all
+Location.destroy_all
+
+10.times do*
+  i=0
+  rue = ['Rue Abel', 'Rue Abel-Gance', 'Rue Abel-Hovelacque', 'Rue Abel-Rabaud', 'Rue Abel-Truchet', 'Rue Achille', 'Rue Achille-Luchaire', 'Rue Achille-Martinet', 'Rue Adolphe-Adam', 'Rue Adolphe-Jullien']
+  zip_adress = [75012, 75013, 75013, 75011, 75017, 75020, 75014, 75018, 75004, 75001]
+  days = [lundi, mardi, mercredi, jeudi, vendredi]
+  Location.create(name:Faker::Fantasy::Tolkien.location, adress_number: (1..10).sample, adress_street: rue.[i], adress_city: 'Paris', zip_code: zip_adress.[i], description: Faker::Lorem.paragraph, day: days.sample, time: time.strftime("%k:%M"), opening_hours: "10h-20h", validated: true)
+  i += 1
+end
+
+5.times do
+  zip = [75001,75002,75003,75004,75005,75006,75007,75008,75009,75010,75011,75012,75013,75014,75015,75016,75017,75018,75019,75020]
+  User.create(first_name: Faker::Games::Pokemon.name, last_name: Faker::Name.last_name, email: Faker::Internet.email, password: "123456", zip_code: zip.sample)
+end
+
+5.times do
+  i = 1
+  Basket.create(title: "Panier #{i}", description: Faker::Lorem.paragraph, price: ((i*2)+10))
+  i += 1
+end
+
+10.times do
+  AdditionalProducts.create(title: Faker::Food.ingredient, description: Faker::Lorem.paragraph, price: (1..8).sample, is_available: Faker::Boolean.boolean(true_ratio: 0.8))
+end
