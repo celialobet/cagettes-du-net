b2627a28e939dc44a3c367c9520a52fefb0e5617
Bapt <baptiste.buch@gmail.com>
2021-06-16 18:46:42 +0200

    first simple view for cart

--- app/models/selection.rb
+++ app/models/selection.rb
@@ -1,5 +1,5 @@
 class Selection < ApplicationRecord
-  belongs_to :basket
+  belongs_to :basket, optional: true
   belongs_to :additional_product, optional: true
   belongs_to :cart
   belongs_to :order, optional: true 
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :carts, path: "mon_panier"
-  resources :baskets
+  
+  resources :baskets do
+    resources :selections
+  end
+
+  resources :additional_products do
+    resources :selections
+  end
 end
