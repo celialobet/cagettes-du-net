1f132d700f1bdf6d1dbea8ac7e1dad8fca0fb0de
Bapt <baptiste.buch@gmail.com>
2021-06-16 16:19:25 +0200

    back cart + routes

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,2 +1,24 @@
 class CartsController < ApplicationController
+  before_action :authenticate_user!
+  
+  def new
+    @cart = Cart.new
+  end
+
+  def create
+    if user_signed_in?
+      @cart = Cart.create(user: current_user)
+      puts "Nouveau panier créé pour #{current_user.first_name}"
+    end
+  end
+
+  def show
+    @cart = Cart.find_by(user_id: current_user.id)
+    @baskets = Basket.all
+    @additional_products = AdditionalProducts.all
+  end
+
+  def index
+
+  end
 end
