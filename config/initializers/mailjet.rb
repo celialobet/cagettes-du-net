5ace7da4303ccbd0a62ba0355cdf66b4ced09be6
Bapt <baptiste.buch@gmail.com>
2021-06-17 12:31:45 +0200

    modif smtp

--- config/initializers/mailjet.rb
+++ config/initializers/mailjet.rb
@@ -2,6 +2,5 @@ require 'mailjet'
 Mailjet.configure do |config|
   config.api_key = ENV['MJ_APIKEY_PUBLIC']
   config.secret_key = ENV['MJ_APIKEY_PRIVATE']
-  config.default_from = 'no-reply-les-cagettes@yopmail.com'
   config.api_version = 'v3.1'
 end
