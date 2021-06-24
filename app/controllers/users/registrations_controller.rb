54e40261fbb33c0eeff12cb81eadee91e8690ea0
Bapt <baptiste.buch@gmail.com>
2021-06-24 23:47:40 +0200

    added authenticate user before action in controllers

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
+  before_action :authenticate_user!
   
   def after_sign_up_path_for(resource)
     :edit_user_registration
