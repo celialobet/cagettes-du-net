116a6cd0b800ed301aa688d874676f1485c61e8c
TimothyREMY <timothy.remy@gmail.com>
2021-06-25 17:17:48 +0200

    adjusting step 2 location + return to step 1

--- app/controllers/additional_products_controller.rb
+++ app/controllers/additional_products_controller.rb
@@ -3,7 +3,7 @@ class AdditionalProductsController < ApplicationController
   def index
     @additional_products = AdditionalProduct.all
     @cart = current_user.cart
-    @cart.step = 1
+   
   end
     
 end
